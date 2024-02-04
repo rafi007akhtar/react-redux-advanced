efa5f2030d03373be5cc6d1bf88789e075ee52a4
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-02-04 20:35:16 +0530

    calling api on cart state change with useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useEffect } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+
+const FIREBASE_URL = "https://react-http-1e532-default-rtdb.firebaseio.com/";
+const CART_JSON_SUFFIX = "cart.json";
+const completeURL = `${FIREBASE_URL}${CART_JSON_SUFFIX}`;
 
 function App() {
-  const ui = useSelector(state => state.ui);
+  const ui = useSelector((state) => state.ui);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(completeURL, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {ui.isCartVisible && <Cart />}
